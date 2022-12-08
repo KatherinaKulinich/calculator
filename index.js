a55c9c41e780e7c70441056eff4097bafb1103fc
KatherinaKulinich <kulinichcatherina@gmail.com>
2022-12-08 18:58:15 +0200

    fixed operations

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const calculator = document.querySelector('form');
 const result = document.querySelector('#result');
 
-const action = ['+', '-', '*', '/'];
+const action = ['+', '-', '*', '/', '%'];
+const values = ['0', '00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'DEL'];
 
 let numberFirst = '';
 let numberSecond = '';
 let sign = '';
 let finish = false;
+
+
 
 
 calculator.addEventListener('click', (event) => {
@@ -15,51 +18,97 @@ calculator.addEventListener('click', (event) => {
     if(event.target.tagName !== 'INPUT') return;
 
     if (value === 'AC') {
-        clearAll()
+        clearAll(0);
         return;
     }
 
-    if (value === 'DEL') {
-
-        if (numberSecond === '') {
-            numberFirst = numberFirst.slice(0, -1);
-            result.innerHTML = numberFirst;
-            return;
-        }
-        numberSecond = numberSecond.slice(0, -1);
-        result.innerHTML = numberSecond;
-    }
-
     
-    if (Number(value >= 0) || value === '00' || value === '.') {
+    if (values.includes(value)) {
 
         if (numberSecond === '' && sign === '') {
+            if (numberFirst.length >= 9) return;
+
+            
+            if (value === '.' && numberFirst.length === 0) {
+                numberFirst = '0' + '.';
+                result.innerHTML = numberFirst;
+                return;
+            } 
+
+            if (value === '.' &&  numberFirst.length > 0) {     
+                if (numberFirst.indexOf('.') === -1) {
+                    numberFirst += value;
+                    result.innerHTML = numberFirst;
+                }         
+                return;
+            }
+
+
+            if (value === 'DEL' ) {
+                numberFirst = numberFirst.slice(0, -1);
+                result.innerHTML = numberFirst;
+                return;
+            }
 
             numberFirst += value;
             result.innerHTML = numberFirst;
+            trimStartZero(numberFirst);
             return;
         }
 
+
+
         if (numberFirst !== '' && numberSecond !== '' && finish) {
+            if (value === 'DEL' ) {
+                numberFirst = String(numberFirst);
+                numberFirst = numberFirst.slice(0, -1);
+                result.innerHTML = numberFirst;
+                return;
+            }
+
+            if (value === '.') {
+                numberSecond = '0' + '.';
+                result.innerHTML = numberSecond;
+                finish = false;
+                return;
+            }
 
             numberSecond = value;
             result.innerHTML = numberSecond;
             finish = false;
+            trimStartZero(numberSecond);
             return;
         }
 
+
+        if (numberSecond.length >= 9) return;
+
+        if (value === '.' && numberSecond.length === 0) {
+            numberSecond = '0' + '.';
+            result.innerHTML = numberSecond;
+            return;
+        } 
+
+        if (value === '.' &&  numberSecond.length > 0) {
+            if (numberSecond.indexOf('.') === -1) {
+                numberSecond += value;
+                result.innerHTML = numberSecond;
+            } 
+            return;
+        }
+
+        if (value === 'DEL' ) {
+            numberSecond = numberSecond.slice(0, -1);
+            result.innerHTML = numberSecond;
+            return;
+        }
+        
         numberSecond += value;
         result.innerHTML = numberSecond;
-
-        console.log(numberFirst, numberSecond, sign);
+        trimStartZero(numberSecond);
         return;
     }
 
-    // if (value === '%') {
-    //     result.innerHTML = result.innerHTML / 100;
-        
-    //     return;
-    // }
 
     if (action.includes(value)) {
         sign = value;
@@ -75,27 +124,34 @@ calculator.addEventListener('click', (event) => {
         switch(sign) {
             case '+':
                 numberFirst = Number(numberFirst) + Number(numberSecond);
-
                 break;
             case '-':
                 numberFirst = numberFirst - numberSecond;
-                // numberFirst = Number(numberFirst) - Number(numberSecond);
                 break;
             case '/':
                 if (numberSecond === '0') {
-                    result.innerHTML = 'error';
-                    numberFirst === '';
-                    numberFirst === '';
+                    clearAll('error');
                     return;
                 }
                 numberFirst = numberFirst / numberSecond;
-                // numberFirst = Number(numberFirst) / Number(numberSecond);
                 break;
             case '*':
                 numberFirst = numberFirst * numberSecond;
-                // numberFirst = Number(numberFirst) * Number(numberSecond);
+                break;
+            case '%':
+                numberFirst = numberFirst / 100;
                 break;
         }
+
+        let numberLength = String(numberFirst).length;
+        if (!Number.isInteger(numberFirst) && numberLength > 5) {
+            numberFirst = numberFirst.toExponential(6);
+        }
+
+        if (Number.isInteger(numberFirst) && numberLength > 7) {
+            numberFirst = numberFirst.toExponential(9);
+        }
+
         finish = true;
         result.innerHTML = numberFirst;
     }
@@ -103,22 +159,21 @@ calculator.addEventListener('click', (event) => {
 
 
 
-function clearAll() {
+
+function clearAll(value) {
     numberFirst = '';
     numberSecond = '';
     sign = '';
     finish = false;
-    result.innerHTML = 0;
+    result.innerHTML = value;
+}
+
+function trimStartZero(numb) {
+    if (/^0+(?=\d)/.test(numb)) {
+        numb = numb.replace(/^0+(?=\d)/, '');
+        result.innerHTML = numb; 
+        return;
+    }
 }
 
 
-
-
-
-// if (value === '.' ) {
-    //     let currentValue = result.innerHTML;
-
-    //     if (currentValue.indexOf('.') === -1) {
-    //         result.innerHTML += value;
-    //     }
-    // }
